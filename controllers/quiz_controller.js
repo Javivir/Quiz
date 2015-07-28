5afad402164d9d156a065126eecc5fb41ba76a4c
Javivir <jj_avi@hotmail.com>
2015-07-28 20:07:23 +0200

    Crear pregunta y Validación
    Tb se actualizado Sequelize a 3.2.0.

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -25,7 +25,7 @@ exports.index = function (req,res) {
 
 	models.Quiz.findAll(where).then(
 	    function(quizes) {
-			res.render('quizes/index.ejs', {quizes: quizes, search: texto_a_buscar});
+			res.render('quizes/index.ejs', {quizes: quizes, search: texto_a_buscar, errors:[] });
 	    }
 	).catch(function(error) {next(error);})
 };
@@ -33,7 +33,7 @@ exports.index = function (req,res) {
 
 // GET /  quizes / :id
 exports.show = function (req,res) {
-	res.render('quizes/show', { quiz:req.quiz})
+	res.render('quizes/show', { quiz:req.quiz, errors:[] })
 };
 
 
@@ -43,6 +43,33 @@ exports.answer = function(req, res) {
 	if (req.query.respuesta === req.quiz.respuesta) {
 		resultado = 'Correcto';
 	} 
-	res.render('quizes/answer', { quiz:req.quiz, respuesta: resultado});
+	res.render('quizes/answer', { quiz:req.quiz, respuesta: resultado, errors:[] });
 };
 
+// GET /  quizes / new
+exports.new = function(req, res) {
+	var quiz = models.Quiz.build(
+			{pregunta: "Pregunta", respuesta: "Respuesta"}
+		);
+
+	res.render('quizes/new', {quiz:quiz, errors:[] });
+};
+
+// GET /  quizes / create
+exports.create = function(req, res) {
+	var quiz = models.Quiz.build( req.body.quiz);
+// guarda en la DB
+	quiz
+	.validate()
+	.then(
+		function(err){
+			if (err) {
+				res.render('quizes/new', {quiz: quiz, errors: err.errors});
+			} else {
+				quiz
+				.save({fields: ["pregunta", "respuesta"]})
+				.then( function(){ res.redirect('/quizes') });
+			}
+		}
+	);
+};
--- routes/index.js
+++ routes/index.js
@@ -5,12 +5,12 @@ var quizController = require('../controllers/quiz_controller');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Quiz' });
+  res.render('index', { title: 'Quiz', errors:[]  });
 });
 
 /* GET author */
 router.get('/author', function(req, res){ 
-	res.render('author', {});
+	res.render('author', {errors:[] });
 
   });
 
@@ -21,6 +21,8 @@ router.param('quizId', quizController.load); 	//autoload :quidId
 router.get('/quizes', quizController.index);
 router.get('/quizes/:quizId(\\d+)', quizController.show);
 router.get('/quizes/:quizId(\\d+)/answer', quizController.answer);
+router.get('/quizes/new', quizController.new);
+router.post('/quizes/create', quizController.create);
 
 
 
